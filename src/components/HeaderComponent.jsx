907c00707470a2cdbf8f771ed910f18422b03a1e
juyun <juyun571@gmail.com>
2024-01-26 18:09:55 +0900

    initial

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -13,7 +13,7 @@ export default class HeaderComponent extends Component {
           <nav className="navbar navbar-expand-md navbar-dark bg-dark">
             <div>
               <a href="https://www.1jo10000jo.link" className="navbar-brand">
-                To-do-listtt
+                To-do-list
               </a>
             </div>
           </nav>
